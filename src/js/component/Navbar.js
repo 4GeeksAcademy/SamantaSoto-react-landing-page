8aab19a3a50df6d15880b9cc9520058e439dea34
Leticia Soto <samantasoto@gmail.com>
2024-10-30 18:46:23 +0000

    Arreglado collapse del navbar

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
             <div className="container">
                 <a className="navbar-brand" href="#">Start Bootstrap</a>
                 <button className="navbar-toggler" itemType="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" itemID="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
